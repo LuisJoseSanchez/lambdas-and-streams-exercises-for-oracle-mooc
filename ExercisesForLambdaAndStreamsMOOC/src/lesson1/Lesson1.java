18d36526d7434fa69143053586a7fab073ffb4e2
LuisJoseSanchez <luisjoseprofe@gmail.com>
2015-07-18 13:16:33 +0200

    Updating exercises of Lesson 1.

--- ExercisesForLambdaAndStreamsMOOC/src/lesson1/Lesson1.java
+++ ExercisesForLambdaAndStreamsMOOC/src/lesson1/Lesson1.java
@@ -123,6 +123,7 @@ public class Lesson1 {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
     /* YOUR CODE HERE */
+    list.forEach(n -> System.out.println(n));
   }
 
   /**
